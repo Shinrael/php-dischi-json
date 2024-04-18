51ce335c04ba4ad62dd719704378ed377a4e337c
Shinrael <mircoelena89@gmail.com>
2024-04-18 16:37:42 +0200

    [add] newTask

--- js/script.js
+++ js/script.js
@@ -5,7 +5,14 @@ createApp({
     return{
       title: 'Lista Dischi',
       apiUrl: 'server.php',
-      list: []
+      list: [],
+      newTask:{
+        title:'',
+        author:'',
+        year:'',
+        poster:'',
+        genre:''
+      }
     }
   },
 
@@ -16,6 +23,20 @@ createApp({
             
             this.list = result.data;
           })
+    },
+    addNewTask(){
+      
+      const data = new FormData();
+      data.append('newTaskTitle', this.newTask.title);
+      data.append('newTaskAuthor', this.newTask.author);
+      data.append('newTaskYear', this.newTask.year);
+      data.append('newTaskPoster', this.newTask.poster);
+      data.append('newTaskGenre', this.newTask.genre);
+
+      axios.post(this.apiUrl, data)
+            .then(result =>{
+              this.list = result.data;
+            })
     }
   },
 
--- server.php
+++ server.php
@@ -6,12 +6,21 @@ $json_string = file_get_contents('dischi.json');
 
 // Codifico la stringa e la trasformo in un elemento PHP
 
-$list = json_decode($json_string);
+$list = json_decode($json_string, true);
 
-/*
+// Prima controlliamo con isser se esiste in post la nuova task, e se esiste la aggiungiamo al file json
 
-
-*/
+if(isset($_POST['newTaskTitle'])){
+  $new_item = [
+    'title' => $_POST['newTaskTitle'],
+    'author' => $_POST['newTaskAuthor'],
+    'year' => $_POST['newTaskYear'],
+    'poster' => $_POST['newTaskPoster'],
+    'genre' => $_POST['newTaskGenre'],
+  ];
+  $list[] = $new_item;
+  file_put_contents('dischi.json', json_encode($list));
+}
 
 // Trasformo l'elemento PHP come se fosse un JSON
 
